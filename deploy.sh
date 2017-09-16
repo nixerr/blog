f4465b93ebcb9c0c825c7626b7a0ce9d43cbad02
Valentin Shilnenkov <valentin.shilnenkov@kaspersky.com>
2017-09-16 11:42:32 +0300

    Скрип для автодеплоя

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
+rm -rf public
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
